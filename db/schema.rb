b1f6f0ab07a5566178aaeba7d227967ddc5743a5
riskiip <muhamadrizki19@student.undip.ac.id>
2020-09-30 23:42:00 +0700

    Config model for instrument and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_163508) do
+ActiveRecord::Schema.define(version: 2020_09_30_163736) do
 
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_163508) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
